e7a948588dc870b11690517f83ca2107e8235cfc
dim131 <dim-los_13@hotmail.com>
2024-08-30 23:55:59 +0200

    Orthocenter el proof  using cyclic quadrilaterals

---
+++ Interactive proofs/orthocenter_cyclic_el/orthocenter_el.js
@@ -0,0 +1,55 @@
+var transitions = {
+  1 : [show("step1")],
+  2 : [
+    show("step2"),
+    show("BE"),
+    show("CZ"),
+    show("H"),
+    show("E"),
+    show("Z"),
+    show("CZB"),
+    show("BEC"),
+  ],
+  3 : [
+    show("step3"),
+    show("AD"),
+    show("D")
+  ],
+  4 : [
+    show("step4"),
+    show("BCEZfill"),
+    show("BCEZcircle"),
+    show("ZE")
+  ],
+  5 : [
+    show("step5"),
+    show("ZEB"),
+    show("ZCB")
+  ],
+  6 : [
+    show("step6"),
+    hide("BCEZfill"),
+    show("AEZHfill"),
+    show("AEZHcircle"),
+    show("AZC")
+  ],
+  7 : [
+    show("step7"),
+    show("BAD"),
+  ],
+  8 : [
+    show("step8"),
+    hide("AEZHfill"),
+    show("AHZ"),
+    show("CHD"),
+  ],
+  9 : [
+    show("step9"),
+    show("AZHfill"),
+    show("ADB"),
+  ],
+  10: [ 
+    show("step10"),
+    hide("AZHfill")
+  ],
+}
